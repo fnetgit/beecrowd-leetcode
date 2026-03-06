e46fbe40c68f5c44c13e1a8273fb999554f3d458
Francisco Neto <netofrancisco.pro@gmail.com>
2026-03-05 22:04:37 -0300

    13 leetcode v2

--- leetcode/python/13.py
+++ leetcode/python/13.py
@@ -5,14 +5,10 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         total = 0
-        nums = []
-        for i in s:
-            nums.append(romans[i])
-        for i in range(len(nums) - 1):
-            if nums[i + 1] > nums[i]:
-                total -= nums[i]
-            elif nums[i + 1] <= nums[i]:
-                total += nums[i]
-        total += nums[-1]
-        print(total)
+        for i in range(len(s) - 1):
+            if romans[s[i + 1]] > romans[s[i]]:
+                total -= romans[s[i]]
+            elif romans[s[i + 1]] <= romans[s[i]]:
+                total += romans[s[i]]
+        total += romans[s[-1]]
         return total
