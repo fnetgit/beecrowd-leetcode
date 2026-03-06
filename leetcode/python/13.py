86bd27767c1b2c92ef76a5cf3a286e69234273d3
Francisco Neto <netofrancisco.pro@gmail.com>
2026-03-05 22:13:05 -0300

    13 leetcode v2.1

--- leetcode/python/13.py
+++ leetcode/python/13.py
@@ -8,7 +8,7 @@ class Solution:
         for i in range(len(s) - 1):
             if romans[s[i + 1]] > romans[s[i]]:
                 total -= romans[s[i]]
-            elif romans[s[i + 1]] <= romans[s[i]]:
+            else:
                 total += romans[s[i]]
         total += romans[s[-1]]
         return total
