9c65ee30b18b658e81b51b481ce2e88c3cb74281
Francisco Neto <netofrancisco.pro@gmail.com>
2026-05-30 19:00:28 -0300

    feat(python): add 13 leetcode v3.0

--- leetcode/python/13.py
+++ leetcode/python/13.py
@@ -5,10 +5,17 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         total = 0
-        for i in range(len(s) - 1):
-            if romans[s[i + 1]] > romans[s[i]]:
-                total -= romans[s[i]]
+        max_saw = 0
+        for c in reversed(s):
+            n = romans[c]
+            if n < max_saw:
+                total -= n
             else:
-                total += romans[s[i]]
-        total += romans[s[-1]]
+                total += n
+                max_saw = n
         return total
+
+
+r = Solution()
+print(r.romanToInt("XXIV"))
+# print(r.romanToInt("XXVI"))
