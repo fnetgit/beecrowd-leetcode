c61b5098ed82c823631b10556dc3c4f0c605da8e
Francisco Neto <netofrancisco.pro@gmail.com>
2026-06-09 16:55:44 -0300

    refactor(leetcode): simplify majority element logic in Python 169

--- leetcode/python/169.py
+++ leetcode/python/169.py
@@ -8,11 +8,8 @@ class Solution:
         for n in nums:
             if count == 0:
                 candidate = n
-                count += 1
-            else:
-                if n == candidate:
-                    count += 1
-                else:
-                    count -= 1
+            count += 1 if n == candidate else - 1
         return candidate
 
+
+#  Boyer-Moore
