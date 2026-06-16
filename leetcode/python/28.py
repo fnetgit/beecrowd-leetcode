a048d07e0b86d1b00bf06ab55d7229c465ed7360
Francisco Neto <netofrancisco.pro@gmail.com>
2026-06-16 16:14:11 -0300

    refactor(leetcode): optimize  Python 28

--- leetcode/python/28.py
+++ leetcode/python/28.py
@@ -3,8 +3,7 @@
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack)):
+        for i in range(len(haystack) - len(needle) + 1):
             if haystack[i : i + len(needle)] == needle:
                 return i
-            continue
         return -1
