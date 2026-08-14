7db32da5c3ffd9247500520a8ce0743d1348c880
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-14 17:27:38 -0300

    feat(beecrowd): create cpp directory and p1000.cpp

---
+++ beecrowd/cpp/p1000.cpp
@@ -0,0 +1,16 @@
+// Hello World!
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "Hello World!\n";
+
+    return 0;
+}
