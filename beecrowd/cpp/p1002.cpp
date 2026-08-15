85c58970c9e0d9d7877d4c4af386abe00fccaeeb
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-14 22:30:51 -0300

    feat(beecrowd): add p1002.cpp

---
+++ beecrowd/cpp/p1002.cpp
@@ -0,0 +1,18 @@
+// Área do Círculo
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(4) << "A=" << 3.14159 * (r * r) << "\n";
+
+    return 0;
+}
