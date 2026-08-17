a8b574d356b8be511acc0f4f96d4a4acfc951159
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-16 23:31:32 -0300

    feat(beecrowd): add p1004.cpp for simple product calculation

---
+++ beecrowd/cpp/p1004.cpp
@@ -0,0 +1,18 @@
+// Produto Simples
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << "PROD = " << a * b << '\n';
+
+    return 0;
+}
