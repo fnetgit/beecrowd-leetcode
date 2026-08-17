e62bd5b2e5acbbc1fafed4ed9f6a2b25f95f6e5c
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-17 00:11:14 -0300

    feat(beecrowd): add p1005.cpp for weighted average calculation

---
+++ beecrowd/cpp/p1005.cpp
@@ -0,0 +1,18 @@
+// Média 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    double a, b;
+    cin >> a >> b;
+    cout << fixed << setprecision(5) << "MEDIA = " << (a * 3.5 + b * 7.5) / 11 << '\n';
+
+    return 0;
+}
