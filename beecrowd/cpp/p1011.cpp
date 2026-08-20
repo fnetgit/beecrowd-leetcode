da9d823efc5143bf71448db07afce202a2587f5d
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-20 18:29:47 -0300

    feat(beecrowd): add p1011.cpp for volume calculation of a sphere

---
+++ beecrowd/cpp/p1011.cpp
@@ -0,0 +1,18 @@
+// Esfera
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int r;
+    cin >> r;
+    cout << fixed << setprecision(3) << "VOLUME = " << (4.0 / 3) * 3.14159 * pow(r, 3) << '\n';
+
+    return 0;
+}
