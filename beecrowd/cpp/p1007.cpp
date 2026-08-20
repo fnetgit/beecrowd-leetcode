ea5aa6ac99babbd64d6f072428a08f89009e4e70
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-20 15:05:31 -0300

    feat(beecrowd): add p1007.cpp and p1008.cpp

---
+++ beecrowd/cpp/p1007.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    cout << "DIFERENCA = " << A * B - C * D << '\n';
+
+    return 0;
+}
---
+++ beecrowd/cpp/p1008.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, w;
+    double p;
+    cin >> n >> w >> p;
+    cout << "NUMBER = " << n << '\n';
+    cout << fixed << setprecision(2) << "SALARY = U$ " << w * p << '\n';
+
+    return 0;
+}
