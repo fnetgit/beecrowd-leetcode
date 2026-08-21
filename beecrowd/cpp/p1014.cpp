2460c551d8cd4b61814d11f0b7d714a8c5776070
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-21 18:54:29 -0300

    feat(beecrowd): add p1014.cpp for fuel consumption calculation

---
+++ beecrowd/cpp/p1014.cpp
@@ -0,0 +1,19 @@
+// Consumo
+
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    int main()
+    {
+
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
+        int c;
+        double d;
+        cin >> c >> d;
+        cout << fixed << setprecision(3) << c / d << " km/l\n";
+
+        return 0;
+    }
