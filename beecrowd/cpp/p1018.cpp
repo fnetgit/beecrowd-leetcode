1abe49507cd7bb030d550ef0ec12b5179021c5c9
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-25 11:08:05 -0300

    feat(beecrowd): add p1018.cpp for currency denomination calculation

---
+++ beecrowd/cpp/p1018.cpp
@@ -0,0 +1,24 @@
+// Cédulas
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int c = n;
+    int nts[] = {100, 50, 20, 10, 5, 2, 1};
+    cout << n << '\n';
+    for (int nt : nts)
+    {
+        cout << c / nt << " nota(s) de R$ " << nt << ",00\n";
+        c = c % nt;
+    }
+
+    return 0;
+}
