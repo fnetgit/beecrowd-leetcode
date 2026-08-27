e2cad3fcb653d328b1ba4a59cf13d56a523bb7de
Francisco Neto <netofrancisco.pro@gmail.com>
2026-08-27 19:01:18 -0300

    perf(beecrowd): change integer arrays to constexpr for better optimization

--- beecrowd/cpp/p1021.cpp
+++ beecrowd/cpp/p1021.cpp
@@ -11,22 +11,22 @@ int main()
 
     double t;
     int cents;
-    int nts[] = {10000, 5000, 2000, 1000, 500, 200};
-    int mds[] = {100, 50, 25, 10, 5, 1};
+    constexpr int nts[] = {10000, 5000, 2000, 1000, 500, 200};
+    constexpr int mds[] = {100, 50, 25, 10, 5, 1};
     cin >> t;
     cents = round(t * 100);
     cout << "NOTAS:\n";
     for (int nt : nts)
     {
-        cout << cents / nt << " nota(s) de R$ " << nt / 100 << ".00\n";
-        cents = cents % nt;
+        cout << cents / nt << " nota(s) de R$ " << fixed << setprecision(2) << nt / 100.0 << '\n';
+        cents %= nt;
     }
 
     cout << "MOEDAS:\n";
     for (int md : mds)
     {
-        cout << cents / md << fixed << setprecision(2) << " moeda(s) de R$ " << md / 100.00 << '\n';
-        cents = cents % md;
+        cout << cents / md << " moeda(s) de R$ " << fixed << setprecision(2) << md / 100.0 << '\n';
+        cents %= md;
     }
     return 0;
 }
