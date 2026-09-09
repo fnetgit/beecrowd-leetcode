51ca60c8bff78c824c5438d97ddbfc440f4058a6
Francisco Neto <netofrancisco.pro@gmail.com>
2026-09-09 17:53:02 -0300

    fix(beecrowd): simplify variable usage in p1018.cpp

--- beecrowd/cpp/p1018.cpp
+++ beecrowd/cpp/p1018.cpp
@@ -11,13 +11,12 @@ int main()
 
     int n;
     cin >> n;
-    int c = n;
     int nts[] = {100, 50, 20, 10, 5, 2, 1};
     cout << n << '\n';
     for (int nt : nts)
     {
-        cout << c / nt << " nota(s) de R$ " << nt << ",00\n";
-        c = c % nt;
+        cout << n / nt << " nota(s) de R$ " << nt << ",00\n";
+        n %= nt;
     }
 
     return 0;
