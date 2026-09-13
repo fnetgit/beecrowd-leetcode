d0f8a9eed15378730edb91e589392a3de3848b06
Francisco Neto <netofrancisco.pro@gmail.com>
2026-09-13 01:13:58 -0300

    refactor(beecrowd-py): update p1014.py

--- beecrowd/python/p1014.py
+++ beecrowd/python/p1014.py
@@ -1,5 +1,6 @@
 # Consumo
 
-distance_traveled = int(input())
-fuel_consumed = float(input())
-print(f'{distance_traveled / fuel_consumed:.3f} km/l')
+import sys
+
+d = sys.stdin.read().split()
+sys.stdout.write(f"{int(d[0]) / float(d[1]):.3f} km/l\n")
